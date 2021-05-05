4d20d2ec8cd052105e0c09e2df3f715db3c7f8fb
eudes-morais <eudes.morais@gmail.com>
2021-05-04 23:00:57 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/devsuperior/eudes/desvendaseudes/DesvendaseudesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eudes.desvendaseudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesvendaseudesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesvendaseudesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/eudes/desvendaseudes/DesvendaseudesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.eudes.desvendaseudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendaseudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
