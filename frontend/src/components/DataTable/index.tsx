95fdc3215040dc38ae393d36013a89a092a01aa6
eudes-morais <eudes.morais@gmail.com>
2021-05-08 16:05:54 -0300

    Pagination - Communication Between Components

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
@@ -6,6 +7,7 @@ import { BASE_URL } from "utils/requests";
 
 function DataTable() {
 
+	const [activePage, setActivePage] = useState(0);
 	const [page, setPage] = useState<SalePage>({
 		last: true,
 		totalPages: 0,
@@ -15,38 +17,45 @@ function DataTable() {
 	});
 
 	useEffect(() => {
-		axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+		axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
 			.then(response => {
 				setPage(response.data);
 			})
-	}, []);
+	}, [activePage]);
+
+	const changePage = (index: number) => {
+		setActivePage(index);
+	}
 
 	return (
-		<div className="table-responsive">
-			<table className="table table-striped table-sm">
-				<thead>
-					<tr>
-						<th>Data</th>
-						<th>Vendedor</th>
-						<th>Clientes visitados</th>
-						<th>Negócios fechados</th>
-						<th>Valor</th>
-					</tr>
-				</thead>
-				<tbody>
-					{page.content?.map(x => (
-						// Atributo de TAG que o REACT necessita para melhor gerenciamento de coleções
-						<tr key={x.id}>
-							<td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
-							<td>{x.seller.name}</td>
-							<td>{x.visited}</td>
-							<td>{x.deals}</td>
-							<td>{x.amount.toFixed(2)}</td>
+		<>
+			<Pagination page={page} onPageChange={changePage} />
+			<div className="table-responsive">
+				<table className="table table-striped table-sm">
+					<thead>
+						<tr>
+							<th>Data</th>
+							<th>Vendedor</th>
+							<th>Clientes visitados</th>
+							<th>Negócios fechados</th>
+							<th>Valor</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+					</thead>
+					<tbody>
+						{page.content?.map(x => (
+							// Atributo de TAG que o REACT necessita para melhor gerenciamento de coleções
+							<tr key={x.id}>
+								<td>{formatLocalDate(x.date, "dd/MM/yyyy")}</td>
+								<td>{x.seller.name}</td>
+								<td>{x.visited}</td>
+								<td>{x.deals}</td>
+								<td>{x.amount.toFixed(2)}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</>
 	);
 }
 
