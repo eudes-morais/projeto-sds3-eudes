0236219bc7a1b09cc179d61c67c363be25158a22
eudes-morais <eudes.morais@gmail.com>
2021-05-08 18:14:22 -0300

    Env Var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,2 +1,2 @@
 // Por não se tratar de um componente react a terminação do arquivo é .ts
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
