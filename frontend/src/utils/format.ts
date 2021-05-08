c5b278a7cb1f6d7b410d74d206ce4ddce3c15100
eudes-morais <eudes.morais@gmail.com>
2021-05-08 15:04:57 -0300

    BarChart Integration

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,5 @@
+// Função que arredonda as casas decimais
+export const round = (value: number, precision: number) => {
+	var multiplier = Math.pow(10, precision || 0);
+	return Math.round(value * multiplier) / multiplier;
+}
